80ab45e89979ea7d84821b007cb772ab76582a1f
St4lV <152074070+St4lV@users.noreply.github.com>
2026-01-26 05:01:53 +0100

    File format selection

--- node/express_utils/class/Download.js
+++ node/express_utils/class/Download.js
@@ -3,12 +3,13 @@ const SSHCommand = require("./SSHCommand");
 class Download {
     constructor(u,f,q){
         this.url = u || "none";
-        this.format = f || "mp4";
+        this.format = f || "none";
         this.quality = q || "best";
     }
 
     async start(){
-        const command = ("../yt-dlp-master/./yt-dlp.sh -P ../node/downloaded "+this.url).toString();
+        const format = this.format !=="none"? "-f "+this.format:"-f best";
+        const command = ("../yt-dlp-master/./yt-dlp.sh -P ../node/downloaded "+this.url+" --no-js-runtimes "+format).toString();
         const dl = new SSHCommand(command);
         const result = await dl.execute();
         return result;
